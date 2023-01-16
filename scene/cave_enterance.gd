6d8defc8efeb370671398d85ff9ccd6e17a8c1f5
Chance <chancecrawford30@gmail.com>
2023-01-15 19:09:56 -0800

    added scene transition var

--- scene/cave_enterance.gd
+++ scene/cave_enterance.gd
@@ -4,10 +4,11 @@ extends Area2D
 # (in this case the cave entrance area 2d node)
 # there will be a new script variable available in the nodes properties
 # which will be labeled as the var name you give to it (New Scene).
-# in this case since we are exporting a packed scene, this script variable will 
-# allow us to select a scene to put into the new_scene variable from the
+# in this case since we are exporting a file selection tool that will
+# allow us to select a scene file (ending in tscn or scn) 
+# to put into the new_scene variable from the
 # cave_entrance's inspector menu (usually menu on right side)
-export(PackedScene) var new_scene
+export(String, FILE, "*.tscn,*.scn") var new_scene
 
 # built in function for godot to get the input from user
 func _input(event):
@@ -15,14 +16,19 @@ func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		# check if the player and this area2D's collision area is overlapping
 		# get_overlapping_bodies returns a list of intersecting PhysicsBody2D
-		# with this area object. Since the only one we have is the player it
-		# will check for the player.
-		# the size function will return true or false with 1 or 0 if there
-		# is a physics body intercepting
-		# checking to make sure the player is overlapping, it will be
-		# 1 if they are
-		if get_overlapping_bodies().size() > 0:
-			next_level()
-			
+		# with this area object. This includes kinematic bodies, static bodies
+		# etc
+		var list_of_bodies = get_overlapping_bodies()
+		print(get_overlapping_bodies())
+		# check each body and get its name. if the name is our player node
+		# then run the next_level function.
+		# we could also use get_overlapping_bodies().size() > 0
+		# if no other static or kinematic body was touching
+		# but this current way checks no matter how many bodies are colliding
+		for body in list_of_bodies:
+			if body.get_name() == "player":
+				next_level()
+
+
 func next_level():
-	print('')
+	get_tree().change_scene(new_scene)
